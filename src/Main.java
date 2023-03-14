106071c173f6638888f425a757692d9bcb5f164c
nursalin <nurikm8688@gmail.com>
2023-03-14 15:06:21 +0300

    nursalin commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("tapshyrma");
+        Nursalin nursalin = new Nursalin();
+        nursalin.name = "Nursalin";
+        nursalin.surname = "Musaev";
+        nursalin.patronymic = "Elbekovich";
+        nursalin.livingPlace = "Sakaldi";
+        nursalin.wasBorn = "2004.07.25";
+        nursalin.howOld = 19;
+        nursalin.whatAreYouDoing = "im Studying";
+        nursalin.hobby = "walk";
+        nursalin.youWeigh = 65;
+        nursalin.street = "Kulmatov 15";
+        System.out.println("name  :" + nursalin.name + "\n" + "surname  :" + nursalin.surname + "\n" + "patronymic : " + nursalin.patronymic + "\n" + "" +
+                "living place :" + nursalin.livingPlace + "\n" + "was born :" + nursalin.wasBorn + "\n" + "how old :" + nursalin.howOld + "\n" + "what Are you doing :" +
+                nursalin.whatAreYouDoing + "\n" + " hobby :" + nursalin.hobby + "\n" + "you weigh :" + nursalin.youWeigh + "\n" + "street :" + nursalin.street);
+
 
     }
 }
--- src/Nursalin.java
+++ src/Nursalin.java
@@ -1,3 +1,12 @@
 public class Nursalin {
-
+    String name;          //аты
+    String surname;       //фамилия
+    String patronymic;    //отчество
+    String livingPlace;   //место жительства
+    String wasBorn;          //дата рождения
+    int howOld;           //сколько лет
+    String whatAreYouDoing; //чем занимаещся
+    String hobby;         //хобби
+    int youWeigh;     //сколька весищ
+    String street;  //улица
 }
